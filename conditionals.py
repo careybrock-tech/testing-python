951dd590f4dfa0d6035c532ad7e50bba622903e6
Carey Brock <careybrock38@gmail.com>
2025-12-18 23:37:04 -0500

    commit if statement

---
+++ conditionals.py
@@ -0,0 +1,5 @@
+# In programming, conditionals are like a fork in the road. They allow your code to make decisions based on whether a condition is True or False.
+# the symbols for conditionals in Python are: ==, !=, >, <, >=, <=
+# The "if" statement is used to execute a block of code if a specified condition is true.
+# The "else" statement is used to execute a block of code if the specified condition is false.
+# The "elif" (short for "else if") statement is used to check multiple conditions in sequence.
