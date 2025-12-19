59727e136c52b53e965c125922c64257da33a53a
Carey Brock <careybrock38@gmail.com>
2025-12-18 21:19:41 -0500

    updated folder

--- hello.py
+++ hello.py
@@ -1,11 +1,8 @@
-# Ask User for their name 
-name = input("What is your name? ")
-# Print a greeting message with the user's name
-print(f"Hello, {name}! Welcome to the program.")
+# print hello world
+print("Hello World!")
 
-# Ask User for their age 
-age = int(input("How old are you? "))   # Convert the input to an integer
-# Print a message indicating the user's age 
-print(f"You are {age} years old.")
-
-
+# print the name from the user
+name = input("What is your name?")
+# remove whitespace from the name and convert to title case
+name = name.strip().title()
+print(f"Hello, {name}")
