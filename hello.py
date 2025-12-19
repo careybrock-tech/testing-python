60d297908644c3d7e10a5c79b834e6480ee273f9
Carey Brock <careybrock38@gmail.com>
2025-12-18 21:05:24 -0500

    third Commit

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 # Ask User for their name 
 name = input("What is your name? ")
 # Print a greeting message with the user's name
-print("Hello, {name}! Welcome to the program.")
+print(f"Hello, {name}! Welcome to the program.")
+
+# Ask User for their age 
+age = int(input("How old are you? "))   # Convert the input to an integer
+# Print a message indicating the user's age 
+print(f"You are {age} years old.")
+
+
