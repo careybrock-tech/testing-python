244256f288f7dd922fcf409d7a5cd6fe126fab33
Carey Brock <careybrock38@gmail.com>
2025-12-19 00:56:30 -0500

    Add or Commit

--- conditionalspart2.py
+++ conditionalspart2.py
@@ -1,9 +1,8 @@
 
-# match is like switch in other languages
-# it is used to check if a value matches one of the cases
 
+# name1 if statement is used to check if a condition is true
+# it is used to execute a block of code if the condition is true
 name1 = input("What is your name ? ")
-
 if name1 == "Harry":
     print("Gryffindor") 
 elif name1 == "Hermione":    
@@ -15,6 +14,8 @@ elif name1 == "Draco":
 else:
     print("Who?")   
 
+# match is like switch in other languages
+# it is used to check if a value matches one of the cases
 name = input("What is your name ? ")
 match name:
     case "Harry":
@@ -27,4 +28,13 @@ match name:
         print("Slytherin")
     case _:
         print("Who?")
-# the _ is a wildcard, it matches any value that doesn't match the previous cases
+
+# name 2 uses or to check if a value is in a list
+# it is used to execute a block of code if the value is in the list
+name2 = input("What is your name ? ")
+if name2 == "Harry" or name2 == "Hermione" or name2 == "Ron":
+    print("Gryffindor")
+elif name2 == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
