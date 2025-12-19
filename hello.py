12013e63504eeccbead479ac8c8a284c0204d8da
Carey Brock <careybrock38@gmail.com>
2025-12-18 21:53:50 -0500

    hello practice

--- hello.py
+++ hello.py
@@ -9,8 +9,16 @@ name = input("What is your name? ").strip().title()
 # print the name of user say hello user
 print(f"Hello {name}")
 
+name1 = input("What is your mom name? ").strip().title()
+# say hello to user again but use the end and sep parameters to print on the same line
+#end parameter is used to add a character at the end of the string
+#sep parameter is used to add a character between the strings
+print(f"Hello {name1}", end="  ", sep="  ")
 # get user Age input and convert it to a integer
 age = int(input("How old are you? "))
 
 # print the Age of the user
 print(f"You are {age} years old.")
+
+# say hello to user double quotes
+print('hello, "friend"')
