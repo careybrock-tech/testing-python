ffc98323d8296170c45d6c2e7faedad86b068635
careybrock-tech <careybrock38@gmail.com>
2025-12-19 01:36:43 -0500

    while loop

--- loops.py
+++ loops.py
@@ -15,7 +15,14 @@
 # i = 3 means it will run 3 times.
 # i -= 1 means it will run 3 times and then stop.
 # if you say negative number there is a infinite loop and it will run forever.
-i = 7 # start the loop at 3
-while i != 0: #true
+#i = 7 # start the loop at 3
+#while i != 0: #true
+#    print("Cool")
+#    i -= 1 # stop the loop after 3 times
+
+# if 1 is less than or equal to 3 then print cool 3 times
+i = 1
+while i <= 3: # print 3 times if 1 <= 3 then print cool 3 times
     print("Cool")
-    i -= 1 # stop the loop after 3 times
+    i += 1 # increment i by 1 each time the loop runs
+
