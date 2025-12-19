4134a08e8dde1217d84a041f174d7d9e428d6c84
Carey Brock <careybrock38@gmail.com>
2025-12-18 21:40:58 -0500

    update file

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 # print hello world
 print(f"Hello world!")
 
+# get user name input remove the white spaces and capitalize the first letter of each word
+name = input("What is your name? ").strip().title()
+# print the name of user
+print(f"Hello {name}")
+
+# get user age input and convert it to a integer
+age = int(input("How old are you? "))
+
+# print the age of the user
+print(f"You are {age} years old.")
