896c357945ad0e3b074d6352fdb62829809a85bc
Carey Brock <careybrock38@gmail.com>
2025-12-18 20:38:42 -0500

    first commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+def main():
+    getUser()
+    print(names)
+
+def getUser():
+    student = ["Carey", "Brock", "John", "Brock"]
+    for s in student:
+        #print(s)
+        return student[]
+
+
+main()
