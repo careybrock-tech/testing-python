354fb034b1bbafc4c629cfdcd72f57813151ad78
Carey Brock <careybrock38@gmail.com>
2025-12-18 20:52:44 -0500

    Updated folder

--- hello.py
+++ hello.py
@@ -1,12 +1,1 @@
-def main():
-    getUser()
-    print(names)
 
-def getUser():
-    student = ["Carey", "Brock", "John", "Brock"]
-    for s in student:
-        #print(s)
-        return student[]
-
-
-main()
